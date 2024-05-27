4d88082b3ec20d701145c662fe744c549868bd95
Tigran Vartanyan <tvartanyan29@yandex.ru>
2024-05-27 15:27:42 +0400

    Api requests/error handling

--- client/src/pages/AuthPage.js
+++ client/src/pages/AuthPage.js
@@ -1,12 +1,35 @@
-import React from "react"
+import React, {useEffect, useState} from "react"
+import { useHttp } from "../hooks/http.hook"
+import { useMessage } from "../hooks/message.hook"
 
 export const AuthPage = () => {
+    const message = useMessage() 
+    const {loading, request, error, clearError} = useHttp()
     const [form, setForm] = useState({
         email: '', password: ''
     })
-
+    useEffect(() => {
+        message(error)
+        clearError()
+    }, [error, message, clearError])
     const changeHandler = event => {
         setForm({...form, [event.target.name]: event.target.value})
+    }
+
+    const registerHandler = async () => {
+        try {
+          const data = await request('/api/auth/register', 'POST', {...form}) 
+          message(data.message)
+          console.log('Data', data) 
+        } catch (e) {}
+    }
+
+    const loginHandler = async () => {
+        try {
+          const data = await request('/api/auth/login', 'POST', {...form}) 
+          message(data.message)
+          console.log('Data', data) 
+        } catch (e) {}
     }
 
     return (
@@ -45,8 +68,21 @@ export const AuthPage = () => {
                         </div>
                     </div>
                     <div className="card-action">
-                        <button className="btn yellow darken-4" style={{marginRight: 10}}>Войти</button>
-                        <button className="btn grey lighten-1 black-text">Регистрация</button>
+                        <button 
+                            className="btn yellow darken-4" 
+                            style={{marginRight: 10}}
+                            disabled={loading}
+                            onClick={loginHandler}
+                        >
+                            Войти
+                        </button>
+                        <button 
+                            className="btn grey lighten-1 black-text"
+                            onClick={registerHandler}
+                            disabled={loading}
+                        >
+                            Регистрация
+                        </button>
                     </div>
                 </div>
             </div>
